d220cf1cf4c784a00b102b44df7c0324d7cb944e
rlemon <rob.lemon@gmail.com>
2012-04-16 17:25:10 -0400

    kinda working session support

--- assets/js/client.js
+++ assets/js/client.js
@@ -2,56 +2,14 @@ var btn_send = By.id('btn-send'),
 	input_text = By.id('input-text'),
 	chat_area = By.id('chat-area');
 
-var last_message = 0,
-	username = null,
-	id = null;
 
 
-
-listener.add(btn_send, 'click', function() {
-	xhr.post({
-		url: '/send',
-		data: {text: input_text.value, name: username}
-	});
-	input_text.value = "";
+var nickname = prompt("Please enter a nickname");
+xhr.request('POST', {
+	url: '/join',
+	data: {nickname: nickname},
+	success: function(res) {
+		console.log(res.responseText);
+	}
 });
 
-function join() {
-	var name = prompt('please enter a username');
-	if( name != '' ) {
-		xhr.post({
-			url: '/join',
-			data: {name: name},
-			success: function(data) {
-				var userdata = JSON.parse(data.responseText);
-				username = userdata.name;
-				id = userdata.id;
-				poll();
-			}
-		});
-	} else {
-		alert('you cannot chat without a name');
-		join();
-	}
-}
-
-function poll() {
-	xhr.post({
-		url: '/recv',
-		data: {'since': last_message},
-		success: function(data) {
-			var messages = JSON.parse(data.responseText);
-			if( messages.length > 0 ) {
-				messages.forEach(function(message) {
-					chat_area.innerHTML += "<div><span>" + message.user + "</span><span>" + message.timestamp + "</span><span>" + message.text + "</span></div>";
-				});
-				last_message = (new Date()).getTime();
-			}
-			setTimeout(poll, 10);
-		},
-		failure: function(){
-			setTimeout(poll, 1000*10);
-		}
-	});
-}
-join();
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ var express = require('express');
 
 var server = express.createServer();
 	//setup server environment
-	server.use(express.cookieParser());
-	server.use(express.session({ secret: "keyboard unicorn" }));
 	server.use(express.bodyParser());
+	server.use(express.cookieParser("keyboard unicorn"));
+	server.use(express.session({ secret: "keyboard unicorn" }));
 	server.use(express.methodOverride());
-	server.use(server.router);
 	server.engine('.html', require('ejs').__express);
 	server.set('views', __dirname + '/lib/views');
 	server.set('view options', {
@@ -18,79 +17,37 @@ var server = express.createServer();
 
 var sessions = [];
 
+server.get('/', function( req, res ) {
+	console.log("sessions", sessions);
+	res.render('index', {
+		'page_title': 'sample title - ' + sessions.length
+	});
+});
 
-// chat room object
+server.get('/room', function( req, res ) {
+	res.render('index', {
+		'page_title': 'sample title #2 - ' + sessions[req.session.id].nickname
+	});
+});
 
-var chatRoom = function() {
-	var messages = [];
-	var users = [];
-	var callbacks = [];
-	var sessions = [];
-	
-	// room public functions
-	this.query = function( since, callback ) {
-		var results = [];
-		messages.forEach(function(message) {
-			if( message.timestamp > since ) {
-				results.push(message);
+server.post('/join', function( req, res ) {
+	var session, nickname = req.param('nickname');
+	if( !sessions[req.session.id] ) {
+		session = {
+			'nickname': nickname,
+			'sid': req.session.id,
+			'timestamp': new Date(),
+			'poke': function() {
+				session.timestamp = new Date();
+			},
+			'destroy': function() {
+				delete sessions[session.sid];
 			}
-		});
-		callback.apply(this, [results]);
-	};
-	
-	this.addUser = function( user, callback ) {
-		if( users.indexOf(user) !== -1 ) {
-			callback.apply(this);
-		}
-		var u = {
-			name: user,
-			id: users.length
-		}
-		users.push(u);
-		callback.apply(this, [u]);
-	};
-	
-	this.addMessage = function( user, message ) {
-		var message = {
-			'user': user,
-			'timestamp': (new Date()).getTime(),
-			'text': message
 		};
-		messages.push(message);
-	};
-};
-
-var room = new chatRoom();
-
-var routes = {
-	root: function(req, res) {
-		res.render('index', {
-			'page_title': 'LD-Tech Chat'
-		});
-	},
-	send: function(req, res) {
-		var text = req.param('text');
-		var name = req.param('name');
-		room.addMessage(name, text);
-		res.send(1);
-	},
-	recv: function(req, res) {
-		var since = req.param('since');
-		room.query(since, function(messages) {
-			res.send(messages);
-		});
-	},
-	join: function(req, res) {
-		var nick = req.param('name');
-		room.addUser(nick, function(data) {
-			res.send(data);
-		});
+		sessions[session.sid] = session;
 	}
-};
-
-server.get('/', routes.root);
-server.post('/send', routes.send);
-server.post('/recv', routes.recv);
-server.post('/join', routes.join);
+	console.log(req.session.id, sessions);
+	res.send(1);
+});
 
 server.listen(4000);
